65505a7d2a0ecb960971f5421397f8b8b59792e3
ashish <ashishpradhan923@gmail.com>
2025-07-27 23:03:35 +0530

    Initialized Portfolio

---
+++ README.md
@@ -0,0 +1,20 @@
+# 🛠️ Akash Meher — Personal Portfolio
+
+Welcome to my personal portfolio website! This project showcases my skills, projects, and experience as a developer. It includes an interactive hero section with particles.js, responsive design, and a clean layout.
+
+---
+
+## 🌐 Live Preview
+
+[🔗 View Portfolio Website](https://portfolio-akashs-projects-36f8e509.vercel.app/)
+
+---
+
+🚀 Features
+⚛️ Animated particle background using particles.js
+
+📱 Fully responsive layout for all devices
+
+🎯 Modern fonts like Space Grotesk / Poppins
+
+🧭 Smooth navigation across sections
---
+++ particle.js
@@ -0,0 +1,45 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 110,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffd700" 
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 2,
+      "random": true
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffff00",
+      "opacity": 0.3,
+      "width": 1.2
+    },
+    "move": {
+      "enable": true,
+      "speed": 2.5
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      }
+    }
+  },
+  "retina_detect": true
+});
